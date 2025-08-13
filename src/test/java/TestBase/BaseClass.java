64b614b84bf394f08947784dcc255e978010cf18
waynethebatmanbale@gmail.com <waynethebatmanbale@gmail.com>
2025-08-13 18:35:56 +0530

    project day-3

--- src/test/java/TestBase/BaseClass.java
+++ src/test/java/TestBase/BaseClass.java
@@ -3,6 +3,7 @@ package TestBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -11,12 +12,16 @@ import java.util.Properties;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -32,9 +37,9 @@ public class BaseClass {
 	public Properties p; 
 
 	     @BeforeClass(groups= {"sanity","regression","master","datadriven"})
-	     @Parameters({"browser"})
+	     @Parameters({"os","browser"})
 	     
-	     void setup( String br) throws IOException
+	     void setup( String os, String br) throws IOException
 	     {
 	    	 // loading config.properties file
 	    	 
@@ -46,21 +51,57 @@ public class BaseClass {
 	    	 
 	    	 logger=LogManager.getLogger(this.getClass());
 	    	 
+	    	 if (p.getProperty("execution_env").equalsIgnoreCase("remote")) 
+	    	 {
+	    	     DesiredCapabilities capabilities = new DesiredCapabilities();
+
+	    	     // os
+	    	     if (os.equalsIgnoreCase("windows")) 
+	    	     {
+	    	         capabilities.setPlatform(Platform.WIN10);
+	    	     } 
+	    	     else if (os.equalsIgnoreCase("linux")) 
+	    	     {
+	    	         capabilities.setPlatform(Platform.LINUX);
+	    	     } 
+	    	     else 
+	    	     {
+	    	         System.out.println("No matching os");
+	    	         return;
+	    	     }
+
+	    	     // browser
+	    	     switch (br.toLowerCase()) 
+	    	     {
+	    	         case "chrome": 
+	    	             capabilities.setBrowserName("chrome"); 
+	    	             break;
+	    	         case "edge": 
+	    	             capabilities.setBrowserName("MicrosoftEdge"); 
+	    	             break;
+	    	         default: 
+	    	             System.out.println("No matching browser"); 
+	    	             return;
+	    	     }
+	    	 }
+
 	    	 
-	    	 
-	    	  switch(br.toLowerCase())
-	    	  {
-	    	  
-	    	  case "chrome" : driver = new ChromeDriver(); break;
-	    	  case "edge" : driver = new EdgeDriver(); break;
-	    	  case "firefox" : driver = new FirefoxDriver(); break;
-	    	  
-	    	  default: System.out.println("invalid browser"); return;
-	    	  		
-	    	  }
-	    	  
-	    	
-	    	 
+	    	 if(p.getProperty("excution_env").equalsIgnoreCase("local"))
+	    	 {
+
+		    	  switch(br.toLowerCase())
+		    	  {
+		    	  
+		    	  case "chrome" : driver = new ChromeDriver(); break;
+		    	  case "edge" : driver = new EdgeDriver(); break;
+		    	  case "firefox" : driver = new FirefoxDriver(); break;
+		    	  
+		    	  default: System.out.println("invalid browser"); return;
+		    	  		
+		    	  } 
+		    	  Capabilities capabilities = null;
+	  driver = new RemoteWebDriver(new URL("http://localhost:44"), capabilities);
+	    	 }
 	    	 
 	    	 
 	    	 driver.manage().deleteAllCookies();
